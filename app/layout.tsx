2e785542cac7fdad94eb49c1603494c96f73c412
moe <blatt@email.com>
2023-11-24 14:42:16 -0500

    basic navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import Navbar from './components/nav/Navbar'
+import Footer from './components/footer/Footer'
 
 const poppins = Poppins({ subsets: ['latin'], 
 weight: ['400', '700'] })
@@ -17,7 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
+      <body className={`${poppins.className} text-slate-700`}>
+        <div className="flex flex-col min-h-screen">
+        <Navbar /> 
+        <main className="flex-grow ">{children}</main>
+        <Footer />
+        </div>
+        </body>
+        </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <main>
+    <div>
     E-Shop
-    </main>
+    </div>
   )
 }
