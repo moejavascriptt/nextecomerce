07a4b36be0858ed51b0dcbafb9063fa1ca52be8c
moe <blatt@email.com>
2023-11-24 23:45:20 -0500

    added products / different data/ other aspects

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
+import { products } from "./utils/products";
+import { truncateText } from "./utils/truncateText";
 
-export default function Home() {
-  return (
+export default function Home(){
+  return ( 
+  <div className="p-8">
+    <Container>
     <div>
-    E-Shop
+    <HomeBanner/>
     </div>
+    <div>
+     {products.map((product: any) => {
+      return <div>{truncateText(product.name)}
+      </div>
+      })}
+    </div>
+    </Container>
+  </div>
   )
 }
