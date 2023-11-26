33e904b271425face19e0bc2b082db9e8f5df908
moe <blatt@email.com>
2023-11-26 16:28:10 -0500

    more pages complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
+import ProductCard from "./components/products/ProductCard";
 import { products } from "./utils/products";
 import { truncateText } from "./utils/truncateText";
+
 
 export default function Home(){
   return ( 
@@ -10,10 +12,10 @@ export default function Home(){
     <div>
     <HomeBanner/>
     </div>
-    <div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4
+    xl-grid-cols-5 2xl:grid-cols-6 gap-8">
      {products.map((product: any) => {
-      return <div>{truncateText(product.name)}
-      </div>
+      return <ProductCard data={product} />
       })}
     </div>
     </Container>
