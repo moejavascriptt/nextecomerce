9fc6a0e5b10b9f6908732881771d5e68fb442a02
moe <blatt@email.com>
2023-11-27 13:31:00 -0500

    made the usecart hook, and used context hook, also wrapped it into our application so it can be used.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import Navbar from './components/nav/Navbar'
 import Footer from './components/footer/Footer'
+import CartProvider from '@/providers/CartProvider'
 
 const poppins = Poppins({ subsets: ['latin'], 
 weight: ['400', '700'] })
@@ -20,11 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className} text-slate-700`}>
+        <CartProvider>
         <div className="flex flex-col min-h-screen">
         <Navbar /> 
         <main className="flex-grow ">{children}</main>
         <Footer />
         </div>
+        </CartProvider>
         </body>
         </html>
   )
