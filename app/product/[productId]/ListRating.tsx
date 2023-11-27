e6316f71e56843c18ef5469f554401d36713f7f0
moe <blatt@email.com>
2023-11-27 13:02:12 -0500

    avatar comp

--- app/product/[productId]/ListRating.tsx
+++ app/product/[productId]/ListRating.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Avatar from "@/app/components/Avatar";
 import Heading from "@/app/components/Heading";
 import { Rating } from "@mui/material";
 import moment from "moment";
@@ -9,13 +10,15 @@ interface ListRatingProps{
 }
 
 const ListRating:React.FC<ListRatingProps> = ({ product}) => {
-    return <div>
+    return ( 
+        <div>
         <Heading title="Product Review"/>
         <div className="text-sm mt-2">
         {product.reviews && product.reviews.map((review: any) => {
             return <div key={review.id}
             className="max-w-300px">
             <div className="flex gap-2 items-center">
+            <Avatar src={review.user.image}/>
             <div className="font-semibold">Avatar</div>
             <div>{review?.user.name}</div>
             <div className="font-light">{moment(review.createdDate).fromNow()}</div>
@@ -27,9 +30,10 @@ const ListRating:React.FC<ListRatingProps> = ({ product}) => {
             <hr className="mt-4 mb-4" />
             </div>
             </div>
+               
         })}
         </div>
     </div>
-}
+)}
 
 export default ListRating;
