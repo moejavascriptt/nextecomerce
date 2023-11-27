49b516ad2a8438c8f5e16c63cd91b5be4258c997
moe <blatt@email.com>
2023-11-27 04:41:24 -0500

    button/ setquantity components

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 
+import Button from "@/app/components/Button";
 import SetColor from "@/app/components/products/SetColor";
+import SetQuantity from "@/app/components/products/SetQuantity";
 import { Rating } from "@mui/material";
 import { useCallback, useState } from "react";
 
@@ -55,6 +57,25 @@ console.log(cartProduct);     const productRating = product.reviews.reduce((acc:
         [cartProduct.selectedImg]
     )
 
+    const handleQtyIncrease = useCallback(() => {
+        if(cartProduct.quantity === 99) {
+            return;
+        }
+
+        setCartProduct((prev) => {
+            return {...prev, quantity: ++prev.quantity}
+        });
+    }, [cartProduct]);
+    const handleQtyDecrease = useCallback(() => {
+        if(cartProduct.quantity === 1) {
+            return;
+        }
+
+         setCartProduct((prev) => {
+            return {...prev, quantity: --prev.quantity}
+         })
+    }, [cartProduct])
+
     return (
     <div className="
     grid
@@ -90,9 +111,17 @@ console.log(cartProduct);     const productRating = product.reviews.reduce((acc:
     handleColorSelect={handleColorSelect}
     />
     <Horizontal />
-    <div>quantity</div>
+    <SetQuantity
+    cartProduct={cartProduct}
+    handleQtyIncrease={handleQtyIncrease}
+    handleQtyDecrease={handleQtyDecrease}
+    />
     <Horizontal />
-    <div>add to cart</div>
+    <div className="max-w-[300px]">
+    <Button 
+    label="Add To Cart"
+    onClick={() => {}}
+    /></div>
     </div>
     </div>
     )
