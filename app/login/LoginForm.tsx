a7087df8b3f3aa72559d5f68c061c0f293ede434
moe <blatt@email.com>
2023-12-01 14:09:13 -0500

    implemented useeffect hooks in the login/register pages so we can render the user to the cart if they are logged in or registereed.

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../components/Heading";
 import Input from "../components/inputs/Input";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
@@ -10,8 +10,13 @@ import { AiOutlineGoogle } from "react-icons/ai";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { SafeUser } from "@/types";
 
-const LoginForm = () => {
+interface LoginFormProps{
+    currentUser: SafeUser | null
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({currentUser}) => {
 
     const [isLoading, setIsLoading] = useState(false)
     const {register, handleSubmit, formState:
@@ -23,9 +28,15 @@ const LoginForm = () => {
         },
     })
 
-    const router = useRouter(
+    const router = useRouter()
 
-    )
+    useEffect(() => {
+    if(currentUser){
+        router.push('/cart')
+        router.refresh();
+    }
+    }, [])
+
     const onSubmit:SubmitHandler<FieldValues> = (data) =>
     {
         setIsLoading(true)
@@ -46,6 +57,10 @@ const LoginForm = () => {
                 }
         })
     }
+
+        if(currentUser){
+            return <p className="text-center">Logged in. Redirecting...</p>
+        }
 
     return (
         <>
