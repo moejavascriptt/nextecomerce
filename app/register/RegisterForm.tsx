73b13d7afaa2c4bbc365bef2f95a668d42ded130
moe <blatt@email.com>
2023-11-30 23:48:43 -0500

    registerfunctionlity/errors/routing/stored w database as well w hashed passwords.

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -7,6 +7,12 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Button from "../components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
+import axios from "axios"
+import toast from "react-hot-toast";
+import {signIn} from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+
 
 const RegisterForm = () => {
 
@@ -20,10 +26,33 @@ const RegisterForm = () => {
         },
     })
 
-    const onSubmit:SubmitHandler<FieldValues> = (data) =>
-    {
+    const router = useRouter()
+
+    const onSubmit:SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true)
-        console.log(data)
+        
+        axios.post('/api/register', data).then(() =>{
+            toast.success("Account created")
+
+            signIn('credentials',{
+                email: data.email, 
+                password: data.password,
+                redirect: false,
+            }).then((callback) => {
+                if(callback?.ok){
+                router.push('/cart')
+                router.refresh()
+                toast.success('Logged in')
+                }
+
+                if(callback?.error){
+                    toast.error(callback.error)
+                }
+            })
+        }).catch(() => toast.error("Something went wrong"))
+        .finally(() => {
+         setIsLoading(false)
+        })
     }
 
     return (
